875aaf1e5e128dbb8ef48fb796fd5f169ce78fe5
Miles Basnillo <milesbasnillo@gmail.com>
2020-06-26 16:17:23 +0800

    Updates

---
+++ precache-manifest.1e0ab7a3850b3c17013a360ea619630e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a49f06c6dc7a6a630365e4e20e1903e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c6cdcb3594403748110",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7fc4910737f84db18bbf",
+    "url": "/static/js/2.12e318de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.12e318de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c6cdcb3594403748110",
+    "url": "/static/js/main.93a2ad07.chunk.js"
+  },
+  {
+    "revision": "a834f63c37edc76171f3",
+    "url": "/static/js/runtime-main.32dd740d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
